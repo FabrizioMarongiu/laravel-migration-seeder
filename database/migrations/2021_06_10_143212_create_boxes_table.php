1be0c47612cef3a51fd4bb4c8bf56d69f5222fdc
FabrizioMarongiu <78483709+FabrizioMarongiu@users.noreply.github.com>
2021-06-10 16:57:21 +0200

    Create Table

---
+++ database/migrations/2021_06_10_143212_create_boxes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBoxesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('boxes', function (Blueprint $table) {
+            $table->id();
+            $table->string('Location', 50);
+            $table->text('Description', 100);
+            $table->text('Duration', 50);
+            $table->boolean('Available')->default(1)
+            $table->mediumInteger('Price')->nullable();
+            $table->tinyInteger('Vote');
+            $table->timestamps();            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('boxes');
+    }
+}
